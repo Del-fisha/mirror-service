b01fdd386d427555e27b6f74b5ee1dfc685fb52d
Tarasov Aleksey <del-fisha@yandex.ru>
2026-04-20 10:06:12 +0300

    chore: add gitignore and remove generated/local files from repository

--- target/classes/data.sql
+++
@@ -1,2 +0,0 @@
-MERGE INTO backup_config (id, backup_path, db_host, db_port, db_name, db_user, db_password, backup_interval_minutes, backup_enabled) KEY (id)
-VALUES (1, './backups', 'localhost', 5432, 'postgres', 'postgres', '', 5, false);
